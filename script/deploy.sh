a70ad32ec4e0b49ef5eff7963205dcbc2678075f
Meron Sibhatu <mertechcloud@gmail.com>
2025-11-02 01:13:47 -0700

    My Feedback App commit

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,24 +1,34 @@
 !#/bin/bash
 
 
+
+
 STACK_NAME="feedback-app"
+
 
 REGION="ca-central-1"
 
+
 Template= "Presigned-S3-URL of the template file from S3"
+
 
 Email="Example@gmail.com"
 
 
+
 aws cloudformation create-stack \
+
 
     --stack-name $STACK_NAME \
 
+
     --template-url templates \
 
-    --parameter-overrides AdminEmail=$EMAIL \
+
+    --parameters ParameterKey=AdminEmail,ParameterValue=$EMAIL \
+
 
     --capabilities CAPABILITY_NAMED_IAM \
 
-    --region $REGION
 
+    --region $REGION
